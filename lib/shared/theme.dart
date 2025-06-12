a72deff7c3e7a13c46572598867cc1f58d7a9852
fuadadhim24 <fuadadhim24@gmail.com>
2025-06-13 01:54:10 +0700

    feat: add theme

---
+++ lib/shared/theme.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+Color blueColor = const Color(0xFF355DEB);
+Color whiteColor = const Color(0xFFFFFFFF);
+Color blackColor = const Color(0xFF212A38);
+Color greyColor = const Color(0xFF8A8F99);
+Color darkGreyColor = const Color(0xFF9dA3Af);
+
+TextStyle blackTextStyle = TextStyle(color: blackColor);
+TextStyle whiteTextStyle = TextStyle(color: whiteColor);
+TextStyle blueTextStyle = TextStyle(color: blueColor);
+TextStyle greyTextStyle = TextStyle(color: greyColor);
+TextStyle darkGreyTextStyle = TextStyle(color: darkGreyColor);
+
+FontWeight lightFontWeight = FontWeight.w300;
+FontWeight regularFontWeight = FontWeight.w400;
+FontWeight mediumFontWeight = FontWeight.w500;
+FontWeight semiBoldFontWeight = FontWeight.w600;
+FontWeight boldFontWeight = FontWeight.w700;
+FontWeight extraBoldFontWeight = FontWeight.w800;
